b21b8a7806ac145f199632d0e2d22ba856470a24
loudeco <louise.decombe@laplateforme.io>
2021-08-09 13:43:50 +0200

    add DataProviders all methods

--- src/DataProvider/DependencyDataProvider.php
+++ src/DataProvider/DependencyDataProvider.php
@@ -1,30 +1,42 @@
 <?php
 
 
-namespace App\DataProvier;
+namespace App\DataProvider;
 
 use ApiPlatform\Core\DataProvider\ContextAwareCollectionDataProviderInterface;
+use ApiPlatform\Core\DataProvider\ItemDataProviderInterface;
 use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
+use ApiPlatform\Core\Exception\ResourceClassNotSupportedException;
 use App\Entity\Dependency;
+use Ramsey\Uuid\Uuid;
 
 class DependencyDataProvider implements ContextAwareCollectionDataProviderInterface,
-    RestrictedDataProviderInterface
+    RestrictedDataProviderInterface, ItemDataProviderInterface
 {
 
-    public function __construct(private string $rootPath) {
+    private string $rootPath;
 
+    public function __construct(string $rootPath) {
+        $this->rootPath = $rootPath;
+    }
+
+    private function getDependencies(): array{
+
+        $path = $this->rootPath . '/composer.json';
+        //   $json = file_get_contents($path);
+        $json = json_decode(file_get_contents($path), true);
+
+            return $json['require'];
     }
 
     public function getCollection(string $resourceClass, string $operationName = null, array $context = [])
     {
 
-        $path = $this->rootPath . '/composer.json';
-     //   $json = file_get_contents($path);
-        $json = json_decode(file_get_contents($path), true)
+
         $items = [];
 
-        foreach ($json['require'] as $name => $version){
-            $items[] = new Dependency('abc', $name, $version);
+        foreach ($this->getDependencies() as $name => $version){
+            $items[] = new Dependency(Uuid::uuid5(Uuid::NAMESPACE_URL, $name)->toString(), $name, $version);
         }
 
         return $items;
@@ -39,4 +51,21 @@ class DependencyDataProvider implements ContextAwareCollectionDataProviderInterf
     }
 
 
+    public function getItem(string $resourceClass, $id, string $operationName = null, array $context = [])
+    {
+
+        $dependencies = $this->getDependencies();
+
+        foreach ($dependencies as $name-> $version) {
+
+            $uuid = Uuid::uuid5(Uuid::NAMESPACE_URL, $name)->toString();
+
+            if ($uuid == $id){
+                return new Dependency($uuid, $name, $version
+                );
+            }
+
+        }
+
+    }
 }
