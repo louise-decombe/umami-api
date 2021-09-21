8aec0e23a94268eb6d52e39d8dcb158b0ffeb300
loudeco <louise.decombe@laplateforme.io>
2021-09-21 10:17:21 +0200

    lexik security / add method count custom route / update datapersister

--- src/Controller/RecipeCountController.php
+++ src/Controller/RecipeCountController.php
@@ -2,18 +2,24 @@
 
 namespace App\Controller;
 
-use App\Entity\Recipe;
 use App\Repository\RecipeRepository;
-use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Component\HttpFoundation\Request;
 
 class RecipeCountController
 {
-    public function __construct(private RecipeRepository $recipeRepository){
+
+    public function __construct(private RecipeRepository $recipeRepository) {
 
     }
 
-    public function __invoke(Request $request): int{
-        return $this->recipeRepository->count([]);
+    public function __invoke(Request $request): int
+    {
+        $onlineQuery = $request->get('online');
+        $conditions = [];
+        if ($onlineQuery !== null) {
+            $conditions = ['online' => $onlineQuery === '1' ? true : false];
+        }
+        return $this->recipeRepository->count($conditions);
     }
+
 }
--- src/DataProvider/DependencyDataProvider.php
+++ src/DataProvider/DependencyDataProvider.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\DataProvider;
 
 use ApiPlatform\Core\DataProvider\ContextAwareCollectionDataProviderInterface;
@@ -8,56 +7,26 @@ use ApiPlatform\Core\DataProvider\ItemDataProviderInterface;
 use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
 use ApiPlatform\Core\Exception\ResourceClassNotSupportedException;
 use App\Entity\Dependency;
+use App\Repository\DependencyRepository;
 use Ramsey\Uuid\Uuid;
 
-class DependencyDataProvider implements ContextAwareCollectionDataProviderInterface,
-    RestrictedDataProviderInterface, ItemDataProviderInterface
+class DependencyDataProvider implements ContextAwareCollectionDataProviderInterface, RestrictedDataProviderInterface, ItemDataProviderInterface
 {
 
-    private string $rootPath;
-
-    public function __construct(string $rootPath) {
-        $this->rootPath = $rootPath;
-    }
-
-    private function getDependencies(): array{
-
-        $path = $this->rootPath . '/composer.json';
-        //   $json = file_get_contents($path);
-        $json = json_decode(file_get_contents($path), true);
-
-            return $json['require'];
-    }
-
-
+    public function __construct(private DependencyRepository $repository) {}
 
     public function supports(string $resourceClass, string $operationName = null, array $context = []): bool
     {
-
-        return $resourceClass == Dependency::class;
-
+        return $resourceClass === Dependency::class;
     }
-
 
     public function getItem(string $resourceClass, $id, string $operationName = null, array $context = [])
     {
-
-        $dependencies = $this->getDependencies();
-
-        foreach ($dependencies as $name-> $version) {
-
-            $uuid = Uuid::uuid5(Uuid::NAMESPACE_URL, $name)->toString();
-
-            if ($uuid == $id){
-                return new Dependency($name, $version);
-            }
-
-        }
-
+        return $this->repository->find($id);
     }
 
     public function getCollection(string $resourceClass, string $operationName = null, array $context = [])
     {
-        // TODO: Implement getCollection() method.
+        return $this->repository->findAll();
     }
 }
