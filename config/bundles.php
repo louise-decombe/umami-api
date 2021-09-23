4ae31d5ef4df5314003def98a6529eed3f15b945
loudeco <louise.decombe@laplateforme.io>
2021-09-23 10:40:19 +0200

    add refresh token config + migration

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
